f209d39993b7d8a45d35479048ba624d06cd60b9
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-30 22:02:20 +0300

    DaisyUI is installed

--- e-commerce/tailwind.config.js
+++ e-commerce/tailwind.config.js
@@ -5,17 +5,24 @@ module.exports = {
     "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      primary: "#252B42",
-      secondary: "#23A6F0",
-      tertiary: "#737373",
-      oldgray: "#BDBDBD",
-      price: "#23856D",
+    extend: {
       star: "#FFCE31",
-      iconorange: "#E77C40",
-      lightgray: "#FAFAFA",
     },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#252B42",
+          secondary: "#23A6F0",
+          accent: "#737373",
+          neutral: "#BDBDBD",
+          success: "#23856D",
+          warning: "#E77C40",
+          info: "#FAFAFA",
+        },
+      },
+    ],
+  },
 };
