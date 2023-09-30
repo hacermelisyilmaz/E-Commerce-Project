45c674e13ef2249454d7fe3ec2795369a963ab4c
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-30 22:02:36 +0300

    Carousel is updated

--- e-commerce/src/components/BannerSlide.js
+++ e-commerce/src/components/BannerSlide.js
@@ -1,40 +1,102 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
 import { useHistory } from "react-router-dom";
 
 function BannerSlide({ data }) {
   const history = useHistory();
   return (
-    <div className="BannerSlide py-12">
-      <Carousel
-        className="border-solid rounded-[1.25rem] h-[38.6rem] mx-14 bg"
-        showThumbs={false}
-      >
-        {data.map((item, index) => {
-          return (
-            <div className="flex justify-end items-center" key={index}>
-              <div className="text-content flex flex-col w-1/2 text-left gap-[1.8rem]">
-                <h5 className="font-bold">{item.season}</h5>
-                <h2 className="font-bold text-6xl">{item.header}</h2>
-                <div className="text-xl">
-                  <p>{item.description1}</p>
-                  <p>{item.description2}</p>
-                </div>
-                <button
-                  className="font-bold text-2xl bg-primary w-1/3 border-solid rounded-[5px] px-10 py-4"
-                  onClick={() => history.push("/productlist")}
-                >
-                  {item.button}
-                </button>
+    <div className="BannerSlide py-12 w-[90%] mx-auto">
+      <div className="carousel rounded-[20px]">
+        <div id="slide1" className="carousel-item relative w-full bg">
+          <div className="flex justify-end items-center w-3/5">
+            <div className="text-content flex flex-col text-left gap-[1.8rem]">
+              <h5 className="font-bold text-secondary">{data[0].season}</h5>
+              <h2 className="font-bold text-6xl">{data[0].header}</h2>
+              <div className="text-xl text-accent">
+                <p>{data[0].description1}</p>
+                <p>{data[0].description2}</p>
               </div>
-              <div className="slide-img w-[38%] right-0">
-                <img src={item.img} />
-              </div>
+              <button
+                className="font-bold text-2xl text-white bg-secondary w-[40%] border-solid rounded-[5px] px-9 py-4"
+                onClick={() => history.push("/productlist")}
+              >
+                {data[0].button}
+              </button>
             </div>
-          );
-        })}
-      </Carousel>
+            {/* <div className="w-[38%]">
+              <img src={data[0].img} />
+            </div> */}
+          </div>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide3" className="btn btn-circle bg-transparent">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle bg-transparent">
+              ❯
+            </a>
+          </div>
+        </div>
+
+        <div id="slide2" className="carousel-item relative w-full">
+          <div className="flex justify-end items-center">
+            <div className="text-content flex flex-col w-1/2 text-left gap-[1.8rem]">
+              <h5 className="font-bold">{data[1].season}</h5>
+              <h2 className="font-bold text-6xl">{data[1].header}</h2>
+              <div className="text-xl">
+                <p>{data[1].description1}</p>
+                <p>{data[1].description2}</p>
+              </div>
+              <button
+                className="font-bold text-2xl text-white bg-secondary w-1/3 border-solid rounded-[5px] px-9 py-4"
+                onClick={() => history.push("/productlist")}
+              >
+                {data[1].button}
+              </button>
+            </div>
+            <div className="slide-img w-[38%] right-0">
+              <img src={data[1].img} />
+            </div>
+          </div>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle bg-transparent">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle bg-transparent">
+              ❯
+            </a>
+          </div>
+        </div>
+
+        <div id="slide3" className="carousel-item relative w-full">
+          <div className="flex justify-end items-center">
+            <div className="text-content flex flex-col w-1/2 text-left gap-[1.8rem]">
+              <h5 className="font-bold">{data[0].season}</h5>
+              <h2 className="font-bold text-6xl">{data[0].header}</h2>
+              <div className="text-xl">
+                <p>{data[0].description1}</p>
+                <p>{data[0].description2}</p>
+              </div>
+              <button
+                className="font-bold text-2xl text-white bg-secondary w-1/3 border-solid rounded-[5px] px-9 py-4"
+                onClick={() => history.push("/productlist")}
+              >
+                {data[0].button}
+              </button>
+            </div>
+            <div className="slide-img w-[38%] right-0">
+              <img src={data[0].img} />
+            </div>
+          </div>
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle bg-transparent">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle bg-transparent">
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
