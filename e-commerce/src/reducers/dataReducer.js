f0e35fed8a526ab8d2646bc9ec60ae4050292abc
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-09-30 22:06:28 +0300

    Data of About page is added to reducer

--- e-commerce/src/reducers/dataReducer.js
+++ e-commerce/src/reducers/dataReducer.js
@@ -2,6 +2,12 @@ export default function dataReducer() {
   return {
     en: {
       hero: {
+        about: {
+          title: "Get answers to all your questions.",
+          description:
+            "Problems trying to resolve the conflict between the two major realms of Classical physics: ",
+          button: "CONTACT OUR COMPANY",
+        },
         header: {
           info: {
             phone: "(225) 555-0118",
