bf95c4875c2d5df0ef62ef60411c0f445578f6da
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-10-05 20:52:25 +0300

    Router is moved to index outside React component. Pages added

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,29 +1,42 @@
 import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import { useSelector } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import ProductList from "./pages/ProductList";
 import About from "./pages/About";
+import Pricing from "./pages/Pricing";
+import Team from "./pages/Team";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
 
 function App() {
   const language = "en";
   const data = useSelector((store) => store[language]);
   return (
     <div className="App text-primary">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home data={data} />
-          </Route>
-          <Route exact path="/about">
-            <About data={data} />
-          </Route>
-          <Route exact path="/productlist">
-            <ProductList data={data} />
-          </Route>
-        </Switch>
-      </Router>
+      <Switch>
+        <Route exact path="/">
+          <Home data={data} />
+        </Route>
+        <Route exact path="/about">
+          <About data={data} />
+        </Route>
+        <Route exact path="/contact">
+          <Contact data={data} />
+        </Route>
+        <Route exact path="/pricing">
+          <Pricing data={data} />
+        </Route>
+        <Route exact path="/productlist">
+          <ProductList data={data} />
+        </Route>
+        <Route exact path="/productlist/:productID">
+          <Product data={data} />
+        </Route>
+        <Route exact path="/team">
+          <Team data={data} />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- e-commerce/src/index.js
+++ e-commerce/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 import { Provider } from "react-redux";
@@ -11,11 +12,13 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
