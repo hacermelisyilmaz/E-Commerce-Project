3950bef00ca297356b79680d85a7d830e65a9926
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-10-08 23:11:47 +0300

    useLocation & useHistory

--- e-commerce/src/components/layout/Header.js
+++ e-commerce/src/components/layout/Header.js
@@ -15,12 +15,15 @@ import {
   faTwitter,
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useHistory } from "react-router-dom";
 
 function Header({ data }) {
   const infoData = data.hero.header.info;
   const navData = data.hero.header.nav;
   const rightData = navData.rightside;
+
+  const location = useLocation();
+  const history = useHistory();
 
   return (
     <div className="Header font-bold">
@@ -48,20 +51,31 @@ function Header({ data }) {
       </div>
       <div className="header-nav flex justify-between items-center py-4 px-6 sm:flex-col">
         <div className="w-fit flex justify-between sm:w-full">
-          <h1 className="text-2xl">{data.brand}</h1>
+          <h1
+            className="text-2xl"
+            onClick={() => {
+              history.push("/");
+            }}
+          >
+            {data.brand}
+          </h1>
           <div className="hidden sm:flex sm:items-center sm:gap-6">
-            <Link to="/">
-              <FontAwesomeIcon icon={faMagnifyingGlass} />
-            </Link>
-            <Link to="/">
-              <FontAwesomeIcon icon={faCartShopping} />
-            </Link>
+            {location.pathname === "/productlist" || (
+              <Link to="/">
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </Link>
+            )}
+            {location.pathname === "/productlist" || (
+              <Link to="/">
+                <FontAwesomeIcon icon={faCartShopping} />
+              </Link>
+            )}
             <Link to="/">
               <FontAwesomeIcon icon={faBars} />
             </Link>
           </div>
         </div>
-        <div className="flex justify-between w-[85%] sm:pt-20 sm:justify-center">
+        <div className="flex justify-between w-[85%] sm:pt-20 sm:justify-center sm:flex-col sm:gap-[1.3rem]">
           <nav className="text-accent flex items-baseline gap-[1.3rem] sm:flex-col sm:items-center sm:text-3xl">
             <Link to="/">{navData.navlinks.home}</Link>
             <Link
@@ -76,7 +90,13 @@ function Header({ data }) {
             <Link to="/contact">{navData.navlinks.contact}</Link>
             <Link to="/team">{navData.navlinks.team}</Link>
           </nav>
-          <div className="nav-right-side text-secondary flex gap-[1rem] sm:hidden">
+          <div
+            className={
+              location.pathname === "/productlist"
+                ? "nav-right-side text-secondary flex gap-[1rem] sm:flex-col sm:text-3xl sm:items-center sm:gap-[1.3rem]"
+                : "nav-right-side text-secondary flex gap-[1rem] sm:hidden"
+            }
+          >
             <Link to="/">
               <FontAwesomeIcon icon={faUser} />
               <span>{rightData.login}</span>
