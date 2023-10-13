ffc6fcc3a4b42a83d6ec159edfc782e940b2f57a
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-10-13 13:53:02 +0300

    Spinner design change according to being page or inline

--- e-commerce/src/components/Spinner.js
+++ e-commerce/src/components/Spinner.js
@@ -1,6 +1,10 @@
-function Spinner() {
+function Spinner({ page }) {
   return (
-    <div className="Spinner">
+    <div
+      className={
+        page ? "Spinner translate-x-[50%] translate-y-[35vh]" : "Spinner"
+      }
+    >
       <div
         className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
         role="status"
