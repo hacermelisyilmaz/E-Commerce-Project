45847a08c1d9ecf2506f3bb698c8154ff5fe7579
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-10-14 15:40:36 +0300

    Product reducer & actions are completed

--- e-commerce/src/reducers/productReducer.js
+++ e-commerce/src/reducers/productReducer.js
@@ -1,5 +1,41 @@
-const initialState = {};
+const initialState = {
+  productList: [],
+  totalProductCount: 0,
+  pageCount: 0,
+  activePage: 1,
+  fetchState: "NOT_FETCHED",
+};
 
-const productReducer = (state = initialState, action) => {};
+const productReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_PRODUCT_LIST:
+      return {
+        ...state,
+        productList: [...state.productList, action.payload],
+      };
+    case SET_TOTAL_PRODUCT_COUNT:
+      return {
+        ...state,
+        totalProductCount: state.totalProductCount + 1,
+      };
+    case SET_PAGE_COUNT:
+      return {
+        ...state,
+        pageCount: action.payload,
+      };
+    case SET_ACTIVE_PAGE:
+      return {
+        ...state,
+        activePage: action.payload,
+      };
+    case SET_FETCH_STATE:
+      return {
+        ...state,
+        fetchState: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 export default productReducer;
