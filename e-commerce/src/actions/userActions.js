f64cec217ca9a9f87ed4dc0157ac90bb5d839849
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-10-14 14:48:42 +0300

    User reducer & actions are completed

---
+++ e-commerce/src/actions/userActions.js
@@ -0,0 +1,32 @@
+export const SET_NAME = "SET_NAME";
+export const SET_EMAIL = "SET_EMAIL";
+export const SET_PASSWORD = "SET_PASSWORD";
+export const SET_ROLE_ID = "SET_ROLE_ID";
+
+export const setName = (name) => {
+  return {
+    type: SET_NAME,
+    payload: name,
+  };
+};
+
+export const setEmail = (email) => {
+  return {
+    type: SET_EMAIL,
+    payload: email,
+  };
+};
+
+export const setPassword = (password) => {
+  return {
+    type: SET_PASSWORD,
+    payload: password,
+  };
+};
+
+export const setRoleID = (roleID) => {
+  return {
+    type: SET_ROLE_ID,
+    payload: roleID,
+  };
+};
--- e-commerce/src/reducers/userReducer.js
+++ e-commerce/src/reducers/userReducer.js
@@ -1,5 +1,42 @@
-const initialState = {};
+import {
+  SET_EMAIL,
+  SET_NAME,
+  SET_PASSWORD,
+  SET_ROLE_ID,
+} from "../actions/userActions.js";
 
-const userReducer = (state = initialState, action) => {};
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  role_id: "",
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_NAME:
+      return {
+        ...state,
+        name: action.payload,
+      };
+    case SET_EMAIL:
+      return {
+        ...state,
+        email: action.payload,
+      };
+    case SET_PASSWORD:
+      return {
+        ...state,
+        password: action.payload,
+      };
+    case SET_ROLE_ID:
+      return {
+        ...state,
+        role_id: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 export default userReducer;
