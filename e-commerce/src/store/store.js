289cbb3c22cbe57d6d000e2ed497b9233e600a9a
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-10-18 22:52:32 +0300

    Logger is installed & added

--- e-commerce/src/store/store.js
+++ e-commerce/src/store/store.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import indexReducer from "./reducers/indexReducer";
 import thunk from "redux-thunk";
-//logger
+import logger from "redux-logger";
 
-export const store = createStore(indexReducer, applyMiddleware(thunk));
+export const store = createStore(indexReducer, applyMiddleware(thunk, logger));
