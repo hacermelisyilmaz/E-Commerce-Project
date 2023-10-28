c6f745e3f98413dbd2d33848045f35fca9eff0de
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-10-28 16:04:19 +0300

    Categories are fetched using reducer action

--- e-commerce/src/components/Categories.js
+++ e-commerce/src/components/Categories.js
@@ -1,28 +1,45 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchCategories } from "../store/actions/productActions";
+import { Link } from "react-router-dom";
 
 function Categories({ data }) {
+  const dispatch = useDispatch();
+  const categories = useSelector(
+    (store) => store.product.categories.categoryList
+  );
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, []);
+
   return (
     <div className="Categories bg-info">
       <div className="py-6 w-[73%] mx-auto flex justify-between sm:flex-col sm:items-center sm:gap-7">
-        <h2 className="text-2xl font-bold">Shop</h2>
+        <h2 className="text-2xl font-bold">{data.title}</h2>
         <div className="flex gap-4 font-bold py-2">
-          <p>Home</p>
+          <p>{data.prev}</p>
           <FontAwesomeIcon icon={faAngleRight} className="text-neutral" />
-          <p className="text-neutral">Shop</p>
+          <p className="text-neutral">{data.title}</p>
         </div>
       </div>
-      <div className="w-3/4 mx-auto flex justify-center gap-4 pb-12 sm:flex-col sm:items-center">
-        {data.map((cat, index) => {
+      <div className="w-3/4 mx-auto flex justify-center flex-wrap gap-4 pb-12 sm:flex-col sm:items-center">
+        {categories.slice(0, 5).map((cat) => {
           return (
-            <div className="relative" key={index}>
+            <Link
+              className="CategoryCard relative max-h-56 max-w-[18%] overflow-hidden"
+              key={cat.id}
+              to={`/shopping/${cat.code.slice(2)}`}
+            >
               <img src={cat.img} className="object-cover" />
-              <div className="bg-[#21212140] w-full h-full absolute top-0 right-0 hover:hidden"></div>
+              <div className="CategoryCardFilter bg-[#21212140] w-full h-full absolute top-0 right-0"></div>
               <div className="w-full h-full absolute top-0 right-0 text-white flex flex-col justify-center items-center gap-2">
-                <h6 className="font-bold">{cat.name}</h6>
-                <p>{cat.noitems}</p>
+                <h6 className="font-bold">{cat.title}</h6>
+                <p>ITEM NO</p>
               </div>
-            </div>
+            </Link>
           );
         })}
       </div>
