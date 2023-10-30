97949619bc99a9a397f6c17e3063126463c529e4
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-10-30 21:41:46 +0300

    Product card is updated a. to the objects from get request

--- e-commerce/src/components/ProductCard.js
+++ e-commerce/src/components/ProductCard.js
@@ -1,13 +1,16 @@
 function ProductCard({ data }) {
   return (
     <div className="ProductCard">
-      <img src={data.img} />
+      <img
+        src={data.img}
+        className="w-[15rem] h-[18.75rem] object-cover sm:w-full sm:h-[26.7rem]"
+      />
       <div className="font-bold flex flex-col items-center gap-3 pt-6 pb-9">
-        <h5>{data.header}</h5>
-        <p className="text-sm leading-6 text-accent">{data.department}</p>
+        <h5>{data.title}</h5>
+        <p className="text-sm leading-6 text-accent">English Department</p>
         <div className="flex gap-2">
-          <span className="text-neutral">{data.oldprice}</span>
-          <span className="text-success">{data.newprice}</span>
+          <span className="text-neutral">$16.48</span>
+          <span className="text-success">$6.48</span>
         </div>
       </div>
     </div>
