dcc3737963ff34d88e373f7284c69dfc9b9d3a44
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-10-30 21:42:16 +0300

    Filter bar is rewritten as form using hook form

--- e-commerce/src/components/Shop.js
+++ e-commerce/src/components/Shop.js
@@ -3,18 +3,51 @@ import {
   faTableCellsLarge,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useDispatch, useSelector } from "react-redux";
+
 import ProductCard from "./ProductCard";
+import { useEffect } from "react";
+import { setProductList } from "../store/actions/productActions";
+import { useForm } from "react-hook-form";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 function Shop({ data }) {
+  const { category } = useParams();
+  const axiosParams = {
+    category: category,
+    search: "",
+    sort: "",
+  };
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: axiosParams,
+    mode: "all",
+  });
+
+  const products = useSelector((store) => store.product.products);
+  const { productList, totalProductCount } = products;
+  const dispatch = useDispatch();
+
   const clickHandler = (e) => {
     e.target.classList.add("bg-secondary");
     e.target.classList.add("text-white");
   };
 
+  const submitHandler = (axiosParams) => {
+    console.log("axios params: ", axiosParams);
+  };
+
+  useEffect(() => {
+    dispatch(setProductList(axiosParams));
+  }, []);
+
   return (
     <div className="Shop">
-      <div className="w-3/4 mx-auto py-6 flex justify-between items-center sm:flex-col sm:items-center sm:gap-6">
-        <p>{data.resultno}</p>
+      <form
+        className="w-3/4 mx-auto py-6 flex justify-between items-center sm:flex-col sm:items-center sm:gap-6"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <p>{data.showing + totalProductCount + data.results}</p>
         <div className="flex gap-2 items-center">
           <p>{data.views}</p>
           <div className="border rounded-md p-4">
@@ -25,26 +58,34 @@ function Shop({ data }) {
           </div>
         </div>
         <input
+          id="search"
           placeholder="Search"
           className="bg-info border border-solid border-neutral rounded-[5px] py-3 pl-4 pr-7"
+          //value={axiosParams.search}
+          {...register("search")}
         />
         <div className="flex gap-3">
           <select
-            id="filter"
-            name="filter"
-            title="filter"
+            id="sort"
             className="bg-info border border-solid border-neutral rounded-[5px] py-3 pl-4 pr-7"
+            defaultValue={data.fopt1}
+            {...register("sort")}
           >
-            <option value={data.fopt1}>{data.fopt1}</option>
+            <option value="popularity">{data.fopt1}</option>
+            <option value="price-asc">{data.fopt2}</option>
+            <option value="price-desc">{data.fopt3}</option>
           </select>
-          <button className="text-white bg-secondary border-0 border-solid rounded-[5px] py-3 px-5">
+          <button
+            type="submit"
+            className="text-white bg-secondary border-0 border-solid rounded-[5px] py-3 px-5"
+          >
             {data.button}
           </button>
         </div>
-      </div>
-      <div className="flex flex-wrap justify-between w-3/4 mx-auto sm:justify-center sm:gap-4">
-        {data.products.map((card, index) => {
-          return <ProductCard data={card} key={index} />;
+      </form>
+      <div className="flex flex-wrap justify-center gap-7 w-3/4 mx-auto sm:flex-col sm:items-center sm:gap-4">
+        {productList.map((card) => {
+          return <ProductCard data={card} key={card.id} />;
         })}
       </div>
       <div className="flex justify-center font-bold text-secondary bg-white w-fit mx-auto border border-solid border-neutral rounded-lg">
