ca71e39fc95fecc01c03385daaefdf33c867dc93
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-01 12:35:05 +0300

    Task 10

--- e-commerce/src/components/Shop.js
+++ e-commerce/src/components/Shop.js
@@ -4,72 +4,112 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
-import ProductCard from "./ProductCard";
-import { useEffect } from "react";
 import { setProductList } from "../store/actions/productActions";
-import { useForm } from "react-hook-form";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import ProductCard from "./ProductCard";
 
 function Shop({ data }) {
-  const { category } = useParams();
-  const axiosParams = {
-    category: category,
-    search: "",
-    sort: "",
-  };
-
-  const { register, handleSubmit } = useForm({
-    defaultValues: axiosParams,
-    mode: "all",
-  });
-
   const products = useSelector((store) => store.product.products);
   const { productList, totalProductCount } = products;
+
   const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
+  const [axiosParams, setAxiosParams] = useState({
+    category: "",
+    search: "",
+    sort: "",
+  });
+
+  console.log("location: ", location);
 
   const clickHandler = (e) => {
     e.target.classList.add("bg-secondary");
     e.target.classList.add("text-white");
   };
 
+  const createQueryString = (queryObject = {}) => {
+    let queryString = Object.keys(queryObject)
+      .filter(
+        (key) =>
+          queryObject[key] &&
+          !(Array.isArray(queryObject[key]) && !queryObject[key].length)
+      )
+      .map((key) => {
+        return Array.isArray(queryObject[key])
+          ? queryObject[key]
+              .map(
+                (item) =>
+                  `${encodeURIComponent(key)}=${encodeURIComponent(item)}`
+              )
+              .join("&")
+          : `${encodeURIComponent(key)}=${encodeURIComponent(
+              queryObject[key]
+            )}`;
+      })
+      .join("&");
+    return queryString ? `?${queryString}` : "";
+  };
+
   const submitHandler = (axiosParams) => {
-    console.log("axios params: ", axiosParams);
+    history.push({
+      pathname: location.pathname,
+      search: createQueryString(axiosParams),
+    });
   };
 
   useEffect(() => {
     dispatch(setProductList(axiosParams));
   }, []);
 
+  useEffect(() => {
+    setAxiosParams({ ...axiosParams, category: location.pathname.slice(10) });
+    setAxiosParams({
+      ...axiosParams,
+      search: location.search.slice(8, location.search.indexOf("&")),
+    });
+    //setAxiosParams({...axiosParams, sort: location.search.slice(8, location.search.indexOf("&"))});
+
+    console.log("axios params: ", axiosParams);
+    dispatch(setProductList(axiosParams));
+  }, [axiosParams]);
+
   return (
     <div className="Shop">
       <form
         className="w-3/4 mx-auto py-6 flex justify-between items-center sm:flex-col sm:items-center sm:gap-6"
-        onSubmit={handleSubmit(submitHandler)}
+        onSubmit={submitHandler}
       >
         <p>{data.showing + totalProductCount + data.results}</p>
         <div className="flex gap-2 items-center">
           <p>{data.views}</p>
-          <div className="border rounded-md p-4">
+          <div className="border rounded-md p-2">
             <FontAwesomeIcon icon={faTableCellsLarge} />
           </div>
-          <div className="border border-info rounded-md p-4">
+          <div className="border border-info rounded-md p-2">
             <FontAwesomeIcon icon={faListCheck} />
           </div>
         </div>
         <input
           id="search"
+          name="search"
           placeholder="Search"
           className="bg-info border border-solid border-neutral rounded-[5px] py-3 pl-4 pr-7"
-          //value={axiosParams.search}
-          {...register("search")}
+          onChange={(e) => {
+            setAxiosParams({ ...axiosParams, search: e.target.value });
+          }}
         />
         <div className="flex gap-3">
           <select
             id="sort"
+            name="sort"
             className="bg-info border border-solid border-neutral rounded-[5px] py-3 pl-4 pr-7"
             defaultValue={data.fopt1}
-            {...register("sort")}
+            onChange={(e) => {
+              setAxiosParams({ ...axiosParams, sort: e.target.value });
+            }}
           >
             <option value="popularity">{data.fopt1}</option>
             <option value="price-asc">{data.fopt2}</option>
