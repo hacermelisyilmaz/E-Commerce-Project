b328fe63b1a0f10913f558b791b6729f4683ebeb
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-23 14:19:47 +0300

    Product get from new endpoint /products/id

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, useHistory, useLocation, Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 import fetchStates from "../store/fetchStates";
@@ -15,20 +15,30 @@ import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
 import Spinner from "../components/Spinner";
 import ProductCard from "../components/ProductCard";
+import axiosInstance from "../api/axiosInstance";
 
 function Product({ data }) {
+  const images = [
+    "public/img/product/bestseller/p1.png",
+    "public/img/product/bestseller/p2.png",
+  ];
+
   const { productID } = useParams();
   const dispatch = useDispatch();
   const history = useHistory();
   const { state } = useLocation();
-  const { productList, fetchState, totalProductCount } = useSelector(
-    (store) => store.product.products
-  );
   const { cart } = useSelector((store) => store.shopping);
-  const [productData] = productList.filter((p) => p.id == productID);
+  const productList = useSelector(
+    (store) => store.product.products.productList
+  );
+
+  const [isLoading, setLoading] = useState(false);
+  const [product, setProduct] = useState({});
+
   const ratingArr = [];
   for (let i = 0; i < 5; i++) {
-    if (i < parseInt(Math.round(productData?.rating))) ratingArr.push(1);
+    if (isLoading && i < parseInt(Math.round(product.rating)))
+      ratingArr.push(1);
     else ratingArr.push(0);
   }
 
@@ -41,98 +51,100 @@ function Product({ data }) {
     console.log(isAvailable);
     isAvailable
       ? dispatch(updateCartItemQuantity(productID, true))
-      : dispatch(addToCart(productData));
+      : dispatch(addToCart(product));
 
     toast.success("You added the product to your basket.");
   };
 
   useEffect(() => {
-    const params = {
-      category: "",
-      filter: "",
-      sort: "",
-      limit: totalProductCount,
-      offset: 0,
-    };
-    dispatch(setProductList(params));
+    axiosInstance
+      .get(`/products/${productID}`)
+      .then((response) => {
+        setProduct(response.data);
+        setLoading(true);
+      })
+      .catch((error) => {
+        toast.error("Fetch failed. Try again");
+      });
   }, [productID]);
 
-  if (fetchState === fetchStates.FETCH_FAILED) {
-    toast.error("Fetch failed. Try again");
-    return <div className="Product"></div>;
-  } else if (fetchState === fetchStates.FETCHED) {
-    return (
-      <div className="Product sm:flex sm:flex-col">
-        <Header data={data} />
+  return (
+    <div className="Product sm:flex sm:flex-col">
+      <Header />
 
-        <div className="bg-info px-44 pb-12 sm:px-8">
-          <div className="py-6">
-            <nav className="py-2 text-sm flex items-center gap-4 sm:justify-center">
-              <i className="fa-solid fa-angle-left selection:text-neutral text-base"></i>
-              <a
-                className="font-bold"
-                onClick={() => {
-                  history.push(`${state.pathname}/${state.search}`);
-                }}
-              >
-                Back
-              </a>
-            </nav>
-          </div>
+      <div className="bg-info px-44 pb-12 sm:px-8">
+        <div className="py-6">
+          <nav className="py-2 text-sm flex items-center gap-4 sm:justify-center">
+            <i className="fa-solid fa-angle-left selection:text-neutral text-base"></i>
+            <a
+              className="font-bold"
+              onClick={() => {
+                history.push(`${state.pathname}/${state.search}`);
+              }}
+            >
+              Back
+            </a>
+          </nav>
+        </div>
 
+        {isLoading && (
           <div className="flex justify-between gap-7 h-[32rem] sm:h-auto sm:flex-col">
             <div className="w-2/3 h-full sm:w-full">
               <div className="carousel w-full h-[80%]">
-                {productData?.images.map((img, index) => {
-                  return (
-                    <div
-                      id={`product${index + 1}`}
-                      key={index}
-                      className="carousel-item relative w-full"
-                    >
-                      <img src={img.url} className="w-full object-cover" />
-                      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a
-                          href="#product2"
-                          className="btn btn-circle bg-transparent"
-                        >
-                          <i
-                            className="fa-solid fa-chevron-left text-5xl"
-                            style={{ color: "#BDBDBD" }}
-                          ></i>
-                        </a>
-                        <a
-                          href="#product2"
-                          className="btn btn-circle bg-transparent"
-                        >
-                          <i
-                            className="fa-solid fa-chevron-right text-5xl"
-                            style={{ color: "#BDBDBD" }}
-                          ></i>
-                        </a>
+                {
+                  /* product?. */ images.map((img, index) => {
+                    return (
+                      <div
+                        id={`product${index + 1}`}
+                        key={index}
+                        className="carousel-item relative w-full"
+                      >
+                        <img src={img.url} className="w-full object-cover" />
+                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                          <a
+                            href="#product2"
+                            className="btn btn-circle bg-transparent"
+                          >
+                            <i
+                              className="fa-solid fa-chevron-left text-5xl"
+                              style={{ color: "#BDBDBD" }}
+                            ></i>
+                          </a>
+                          <a
+                            href="#product2"
+                            className="btn btn-circle bg-transparent"
+                          >
+                            <i
+                              className="fa-solid fa-chevron-right text-5xl"
+                              style={{ color: "#BDBDBD" }}
+                            ></i>
+                          </a>
+                        </div>
                       </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })
+                }
               </div>
               <div className="flex w-full py-5 gap-5">
-                {productData?.images.map((img, index) => {
-                  return (
-                    <a
-                      href={`#product${index + 1}`}
-                      key={index}
-                      className="w-24 h-20 overflow-hidden"
-                    >
-                      <img src={img.url} className="w-full object-cover" />
-                    </a>
-                  );
-                })}
+                {
+                  /* product?. */ images.map((img, index) => {
+                    return (
+                      <a
+                        href={`#product${index + 1}`}
+                        key={index}
+                        className="w-24 h-20 overflow-hidden"
+                      >
+                        <img src={img.url} className="w-full object-cover" />
+                      </a>
+                    );
+                  })
+                }
               </div>
             </div>
 
             <div className="p-6 pt-3">
               <h3 className="text-[1.25rem] leading-[1.875rem]">
-                {productData?.name}
+                {product.name}
               </h3>
               <div className="mt-3 flex gap-3 items-center">
                 <div className="flex gap-1">
@@ -149,16 +161,14 @@ function Product({ data }) {
                   })}
                 </div>
                 <p className="text-sm text-accent font-bold">
-                  {productData?.rating} / 5.00
+                  {product.rating} / 5.00
                 </p>
               </div>
-              <h4 className="mt-5 text-2xl font-bold">
-                {productData?.price} ₺
-              </h4>
+              <h4 className="mt-5 text-2xl font-bold">{product?.price} ₺</h4>
               <div className="mt-1 text-sm leading-6 font-bold text-accent">
                 <span>In Stock</span>
               </div>
-              <p className="mt-8 text-sm">{productData?.description}</p>
+              <p className="mt-8 text-sm">{product?.description}</p>
               <hr className="my-7" />
               <img src="/img/posts/product-colors.png" />
               <div className="mt-16 flex items-center gap-2">
@@ -180,111 +190,103 @@ function Product({ data }) {
               </div>
             </div>
           </div>
-        </div>
+        )}
+      </div>
 
-        <div className="px-44 sm:px-8">
-          <div className="py-3 flex justify-center font-bold text-accent">
-            <Link to="#" className="p-6">
-              Description
-            </Link>
-            <Link to="#" className="p-6">
-              Additional Information
-            </Link>
-            <Link to="#" className="p-6">
-              Reviews
-            </Link>
-          </div>
-          <hr className="pb-4" />
-          <div className="pt-6 flex justify-between sm:flex-col">
-            <img src="/img/product/desc.png" className="object-contain" />
-            <div className="flex flex-col gap-7 mx-7 w-1/3 sm:w-full sm:mx-0 sm:my-6">
-              <h5 className="text-2xl font-bold">"the quick fox jumps over"</h5>
-              <div className="text-sm text-accent flex flex-col gap-5">
-                <p>
-                  Met minim Mollie non desert Alamo est sit cliquey dolor do met
-                  sent. RELIT official consequent door ENIM RELIT Mollie.
-                  Excitation venial consequent sent nostrum met.
-                </p>
-                <p>
-                  Met minim Mollie non desert Alamo est sit cliquey dolor do met
-                  sent. RELIT official consequent door ENIM RELIT Mollie.
-                  Excitation venial consequent sent nostrum met.
-                </p>
-                <p>
-                  Met minim Mollie non desert Alamo est sit cliquey dolor do met
-                  sent. RELIT official consequent door ENIM RELIT Mollie.
-                  Excitation venial consequent sent nostrum met.
-                </p>
-              </div>
+      <div className="px-44 sm:px-8">
+        <div className="py-3 flex justify-center font-bold text-accent">
+          <Link to="#" className="p-6">
+            Description
+          </Link>
+          <Link to="#" className="p-6">
+            Additional Information
+          </Link>
+          <Link to="#" className="p-6">
+            Reviews
+          </Link>
+        </div>
+        <hr className="pb-4" />
+        <div className="pt-6 flex justify-between sm:flex-col">
+          <img src="/img/product/desc.png" className="object-contain" />
+          <div className="flex flex-col gap-7 mx-7 w-1/3 sm:w-full sm:mx-0 sm:my-6">
+            <h5 className="text-2xl font-bold">"the quick fox jumps over"</h5>
+            <div className="text-sm text-accent flex flex-col gap-5">
+              <p>
+                Met minim Mollie non desert Alamo est sit cliquey dolor do met
+                sent. RELIT official consequent door ENIM RELIT Mollie.
+                Excitation venial consequent sent nostrum met.
+              </p>
+              <p>
+                Met minim Mollie non desert Alamo est sit cliquey dolor do met
+                sent. RELIT official consequent door ENIM RELIT Mollie.
+                Excitation venial consequent sent nostrum met.
+              </p>
+              <p>
+                Met minim Mollie non desert Alamo est sit cliquey dolor do met
+                sent. RELIT official consequent door ENIM RELIT Mollie.
+                Excitation venial consequent sent nostrum met.
+              </p>
             </div>
-            <div className="font-bold">
-              <div className="flex flex-col gap-7">
-                <h5 className="text-2xl">the quick fox jumps over</h5>
-                <div className="flex flex-col gap-2">
-                  <div className="flex text-sm text-accent gap-5">
-                    <i className="fa-solid fa-angle-right text-base"></i>
-                    <p>the quick fox jumps over the lazy dog</p>
-                  </div>
-                  <div className="flex text-sm text-accent gap-5">
-                    <i className="fa-solid fa-angle-right text-base"></i>
-                    <p>the quick fox jumps over the lazy dog</p>
-                  </div>
-                  <div className="flex text-sm text-accent gap-5">
-                    <i className="fa-solid fa-angle-right text-base"></i>
-                    <p>the quick fox jumps over the lazy dog</p>
-                  </div>
-                  <div className="flex text-sm text-accent gap-5">
-                    <i className="fa-solid fa-angle-right text-base"></i>
-                    <p>the quick fox jumps over the lazy dog</p>
-                  </div>
+          </div>
+          <div className="font-bold">
+            <div className="flex flex-col gap-7">
+              <h5 className="text-2xl">the quick fox jumps over</h5>
+              <div className="flex flex-col gap-2">
+                <div className="flex text-sm text-accent gap-5">
+                  <i className="fa-solid fa-angle-right text-base"></i>
+                  <p>the quick fox jumps over the lazy dog</p>
                 </div>
-              </div>
-              <div className="flex flex-col gap-7 pt-6">
-                <h5 className="text-2xl">the quick fox jumps over</h5>
-                <div className="flex flex-col gap-2">
-                  <div className="flex text-sm text-accent gap-5">
-                    <i className="fa-solid fa-angle-right text-base"></i>
-                    <p>the quick fox jumps over the lazy dog</p>
-                  </div>
-                  <div className="flex text-sm text-accent gap-5">
-                    <i className="fa-solid fa-angle-right text-base"></i>
-                    <p>the quick fox jumps over the lazy dog</p>
-                  </div>
-                  <div className="flex text-sm text-accent gap-5">
-                    <i className="fa-solid fa-angle-right text-base"></i>
-                    <p>the quick fox jumps over the lazy dog</p>
-                  </div>
+                <div className="flex text-sm text-accent gap-5">
+                  <i className="fa-solid fa-angle-right text-base"></i>
+                  <p>the quick fox jumps over the lazy dog</p>
+                </div>
+                <div className="flex text-sm text-accent gap-5">
+                  <i className="fa-solid fa-angle-right text-base"></i>
+                  <p>the quick fox jumps over the lazy dog</p>
+                </div>
+                <div className="flex text-sm text-accent gap-5">
+                  <i className="fa-solid fa-angle-right text-base"></i>
+                  <p>the quick fox jumps over the lazy dog</p>
                 </div>
               </div>
             </div>
+            <div className="flex flex-col gap-7 pt-6">
+              <h5 className="text-2xl">the quick fox jumps over</h5>
+              <div className="flex flex-col gap-2">
+                <div className="flex text-sm text-accent gap-5">
+                  <i className="fa-solid fa-angle-right text-base"></i>
+                  <p>the quick fox jumps over the lazy dog</p>
+                </div>
+                <div className="flex text-sm text-accent gap-5">
+                  <i className="fa-solid fa-angle-right text-base"></i>
+                  <p>the quick fox jumps over the lazy dog</p>
+                </div>
+                <div className="flex text-sm text-accent gap-5">
+                  <i className="fa-solid fa-angle-right text-base"></i>
+                  <p>the quick fox jumps over the lazy dog</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
 
-        <div className="bg-info py-12 px-44 flex flex-col gap-6 sm:px-10 sm:items-center">
-          <h3 className="text-2xl font-bold">BESTSELLER PRODUCTS</h3>
-          <hr />
+      <div className="bg-info py-12 px-44 flex flex-col gap-6 sm:px-10 sm:items-center">
+        <h3 className="text-2xl font-bold">BESTSELLER PRODUCTS</h3>
+        <hr />
+        {isLoading && (
           <div className="flex flex-wrap justify-between sm:justify-center">
-            {productList.slice(0, 8).map((product, index) => {
+            {productList?.slice(0, 8).map((product, index) => {
               return <ProductCard key={index} data={product} />;
             })}
           </div>
-        </div>
+        )}
+      </div>
 
-        <Clients bg={true} />
-        <Footer data={data} inner={true} />
-      </div>
-    );
-  } else if (fetchState === fetchStates.FETCHING) {
-    return (
-      <div className="Product py-48 flex justify-center">
-        <Spinner />
-      </div>
-    );
-  } else {
-    toast.error("Product not found.");
-    history.goBack();
-    return <div className="Product"></div>;
-  }
+      <Clients bg={true} />
+      <Footer data={data} inner={true} />
+    </div>
+  );
 }
 
 export default Product;
